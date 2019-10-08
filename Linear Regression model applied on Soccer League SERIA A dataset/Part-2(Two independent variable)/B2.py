677679c9840f263fb1fdad55475918b5db05b6dc
sjangir1 <45509448+sjangir1@users.noreply.github.com>
2019-10-08 10:27:41 -0400

    Create B2.py

---
+++ Linear Regression model applied on Soccer League SERIA A dataset/Part-2(Two independent variable)/B2.py
@@ -0,0 +1,40 @@
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import linear_model
+from sklearn.model_selection import train_test_split
+
+
+df=pd.read_csv("S:/AML/AML-Assignment-2/Part-2/Dataset.csv")
+print(df)
+
+X=df[['AverageAge','Foreigners']]
+y=df['Points']
+
+regression=linear_model.LinearRegression()
+
+i=0
+for i in range(0,20):
+    X_train, X_test, y_train, y_test=train_test_split(X,y,test_size=0.5)
+    regression.fit(X_train,y_train)
+    t=regression.predict(X_test)
+    print(X_test)
+    print(t)
+    i+=1
+
+d=pd.read_csv('S:/AML/AML-Assignment-2/Part-2/dataset20experiment.csv')
+points=d['Points']
+predicted=d['Predicted Points']
+
+accuracy = regression.score(df[['AverageAge','Foreigners']],points)
+print(accuracy*100,'%')
+
+plt.plot(points,'*',
+        predicted,'.')
+plt.show()    # Orange dots are the Predicted Points and Blue Stars are the Expected/Actual Points
+
+
+
+
+
